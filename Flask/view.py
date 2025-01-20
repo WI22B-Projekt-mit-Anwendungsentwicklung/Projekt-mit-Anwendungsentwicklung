dee50346141019aa3e2da5b948c1c934f4fa94f3
Marc <marc.schuler.ms@outlook.com>
2025-01-20 12:28:03 +0100

    Combination of two old repositories

--- Flask/view.py
+++ Flask/view.py
@@ -1,5 +1,6 @@
-from Flask import Flask, render_template
+from flask import Flask, render_template
 import os
+import mysql.connector
 
 app = Flask(__name__)
 
@@ -13,3 +14,14 @@ if __name__ == "__main__":
     port = int(os.environ.get('PORT', 4999))
     app.run(debug=True, host='0.0.0.0', port=port)
 
+
+
+connection = mysql.connector.connect(
+    user='root', password='root', host='mysql', port="3306", database='db')
+print("DB connected")
+
+cursor = connection.cursor()
+cursor.execute("SELECT * FROM students")
+students = cursor.fetchall()
+connection.close()
+print(students)
