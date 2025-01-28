445884160ff3859c40e2b8e9af1dffae7aff5a35
Marc <marc.schuler.ms@outlook.com>
2025-01-28 11:25:32 +0100

    Fix for Google API

--- App/app.py
+++ App/app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request, jsonify
-from flask_restful import Resource, Api
 import mysql.connector
 import station
 import datapoint as dp
@@ -28,16 +27,16 @@ if not inhalt_station:
 else:
     print("Tabelle bereits gefüllt")
 
-cursor.execute("SELECT * FROM Datapoint;")
-inhalt_datapoint = cursor.fetchall()
-if not inhalt_datapoint and inhalt_station:
-    print("Tabelle Datapoint leer")
-    for station in inhalt_station:
-        datapoints = dp.download_and_create_datapoints(station[0])
-        for datapoint in datapoints:
-            cursor.execute(f"INSERT INTO Datapoint (station_id, year, month, tmax, tmin) VALUES ('{datapoint.station}', {str(datapoint.date)[:4]}, {str(datapoint.date)[-2:]},{datapoint.tmax},{datapoint.tmin});")
-else:
-    print("Tabelle bereits gefüllt")
+##cursor.execute("SELECT * FROM Datapoint;")
+##inhalt_datapoint = cursor.fetchall()
+##if not inhalt_datapoint and inhalt_station:
+##    print("Tabelle Datapoint leer")
+##    for station in inhalt_station:
+##        datapoints = dp.download_and_create_datapoints(station[0])
+##        for datapoint in datapoints:
+##            cursor.execute(f"INSERT INTO Datapoint (station_id, year, month, tmax, tmin) VALUES ('{datapoint.station}', {str(datapoint.date)[:4]}, {str(datapoint.date)[-2:]},{datapoint.tmax},{datapoint.tmin});")
+##else:
+##    print("Tabelle bereits gefüllt")
 
 
 cursor.execute("SELECT * FROM Station LIMIT 10;")
