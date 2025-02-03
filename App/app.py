fd7c6e95d988bf28800ce39315ca793d018afa86
lukasschick <schick_lukas@t-online.de>
2025-02-03 12:54:21 +0100

    MySQL logic for import from local file.

--- App/app.py
+++ App/app.py
@@ -17,7 +17,6 @@ cursor = connection.cursor()
 cursor.execute("SELECT * FROM Station;")
 inhalt_station = cursor.fetchall()
 if not inhalt_station:
-    print("Tabelle leer")
     stations = station.load_stations_from_url("https://www1.ncdc.noaa.gov/pub/data/ghcn/daily/ghcnd-inventory.txt")
     for station in stations:
         cursor.execute("INSERT INTO Station (id, latitude, longitude, first_tmax, latest_tmax, first_tmin, latest_tmin) "
@@ -27,23 +26,20 @@ if not inhalt_station:
 else:
     print("Tabelle bereits gefüllt")
 
-cursor.execute("SELECT * FROM Datapoint;")
+cursor.execute("SELECT * FROM Datapoint LIMIT 1;")
 inhalt_datapoint = cursor.fetchall()
-if not inhalt_datapoint and inhalt_station:
-    print("Tabelle Datapoint leer")
+if not inhalt_datapoint:
     for station in inhalt_station:
         datapoints = dp.download_and_create_datapoints_local(station[0])
         for datapoint in datapoints:
             cursor.execute(f"INSERT INTO Datapoint (station_id, year, month, tmax, tmin) VALUES ('{datapoint.station}', {str(datapoint.date)[:4]}, {str(datapoint.date)[-2:]},{datapoint.tmax},{datapoint.tmin});")
+    connection.commit()
 else:
     print("Tabelle bereits gefüllt")
-
-
-cursor.execute("SELECT * FROM Datapoint LIMIT 10;")
-rows = cursor.fetchall()
+cursor.execute("SELECT * FROM Datapoint LIMIT 1;")
+result = cursor.fetchall()
+print(result)
 connection.close()
-
-print(rows)
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=8000)
