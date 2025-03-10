65d02aba2c15a981fa2e25d151535139ecda7046
leonr <leon.rebholz@gmail.com>
2025-03-10 17:51:51 +0100

    Bug Fixes for Testing

--- App/Tests/wait-for-app.sh
+++ App/Tests/wait-for-app.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "⏳ Waiting for Flask app to be ready..."
-while ! curl -s http://localhost:8000/ > /dev/null; do
+while ! curl -s http://0.0.0.0:8000/ > /dev/null; do
   sleep 1
 done
 echo "✅ Flask app is ready! Running tests..."
