dd75c76e369b3b39cca44226ba8b8164ae552fdf
leonr <leon.rebholz@gmail.com>
2025-03-10 17:04:56 +0100

    Timer

---
+++ App/Tests/wait-for-app.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo "⏳ Waiting for Flask app to be ready..."
+while ! curl -s http://localhost:8000/ > /dev/null; do
+  sleep 1
+done
+echo "✅ Flask app is ready! Running tests..."
+
+pytest App/Tests/
