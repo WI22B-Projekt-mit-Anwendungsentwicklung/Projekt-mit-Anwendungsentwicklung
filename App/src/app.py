f639dfe9457e531928f33be92953139fac8c3729
Lefi1402 <leon.rebholz@gmail.com>
2025-03-13 18:41:16 +0100

    Update app.py

--- App/src/app.py
+++ App/src/app.py
@@ -2,12 +2,12 @@ from flask import Flask
 from flask_cors import CORS
 from routes import init_routes
 import data_services as ds
-#from tests import run_all_tests
+from tests import run_all_tests
 app = Flask(__name__)
 CORS(app)
 init_routes(app)
 ds.save_data_to_db()
-#run_all_tests()
+run_all_tests()
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
